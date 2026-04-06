a54021bfeed932aa2bb1a950d995c9daefcaa6ce
Chamika Dasun <156738794+dasunc23@users.noreply.github.com>
2026-04-06 21:37:32 +0530

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Nethmi's pages
+import Home from './Pages/Home';
+import AdminDashboard from './Pages/AdminDashboard';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+
+// Your pages
 import CreateRoom from './Pages/CreateRoom';
 import JoinRoom from './Pages/JoinRoom';
 import Lobby from './Pages/Lobby';
 
-
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/create" element={<CreateRoom />} />
         <Route path="/join" element={<JoinRoom />} />
         <Route path="/lobby/:roomCode" element={<Lobby />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
